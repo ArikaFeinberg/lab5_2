fd44ba1a69788124bffdfc3caf44b8a95aaa1df7
ArikaFeinberg <dmolodec@gmail.com>
2019-12-27 15:15:13 +0200

    fix_bugs

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var r = 10; a = 3; b = 4/*Ініціюються дві змінні*/
-document.write ("Площа круга = ", Math.PI*r*r, "<br /><br />")
-document.write ("Гіпотенуза прмяокутного трикутника = ", Math.sqrt(a*a+b*b))
+var r = 10;
+a = 3;
+b = 4; /*Ініціюються дві змінні*/
+document.write('Площа круга = ', Math.PI * r * r, '<br /><br />');
+document.write(
+    'Гіпотенуза прмяокутного трикутника = ',
+    Math.sqrt(a * a + b * b)
+);
